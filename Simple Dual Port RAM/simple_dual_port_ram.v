315bd6f6a342df11cc24717fc50f12b0c5fe0eea
Tanuku Gnanendra Bala Chandra Sekhar <23a95a0426@aec.edu.in>
2026-03-10 23:51:21 +0530

    Create simple_dual_port_ram.v

---
+++ Simple Dual Port RAM/simple_dual_port_ram.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+
+module dual_port_ram(
+        clk,
+        we,
+        w_addr,
+        write_data,
+        r_addr,
+        read_data
+    );
+    parameter ADDR_WIDTH = 10;
+    parameter DATA_WIDTH = 16;
+    
+    input clk;
+    // Write Port
+    input we;
+    input [ADDR_WIDTH-1:0]w_addr;
+    input [DATA_WIDTH-1:0]write_data;
+    // Read Port
+    input      [ADDR_WIDTH-1:0]r_addr;
+    output reg [DATA_WIDTH-1:0]read_data;
+    
+    localparam DEPTH = 1 << ADDR_WIDTH;
+    reg [DATA_WIDTH-1:0] mem [0:DEPTH-1];
+    
+    always@(posedge clk) begin
+        // Write Operation
+        if(we) 
+            mem[w_addr] <= write_data;
+        // Read Operation
+        read_data <= mem[r_addr];
+    end
+endmodule
